8c6d3baaad9be87e874c3aa4c83bc67775e54358
GustavoFreitas22 <gusfmotta@gmail.com>
2021-07-05 13:18:22 -0300

    Projeto v1.0

---
+++ java/AULAS/src/introducao/ExemploWhile.java
@@ -0,0 +1,31 @@
+package introducao;
+
+import java.util.*;
+
+public class ExemploWhile {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		
+		int numero;
+		int cont=1;
+		int aux = 1;
+		
+		System.out.println("Digite um numero positivo: ");
+		numero = read.nextInt();
+		
+		if(numero<=0) {
+			System.out.println("Numero invalido!");
+		}else {
+			do {
+				System.out.printf("%d + ", cont);
+				cont ++;
+				aux = aux +cont;
+				
+			}while(cont < numero);
+			System.out.println(numero+" = "+aux);
+		}
+		
+	}
+
+}
