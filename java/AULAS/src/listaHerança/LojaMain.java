297608d0ff81028debfdaeeb1d4d1b40f1759f82
GustavoFreitas22 <gusfmotta@gmail.com>
2021-07-13 16:58:37 -0300

    Lista Collections

---
+++ java/AULAS/src/listaHerança/LojaMain.java
@@ -0,0 +1,34 @@
+package listaHerança;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LojaMain {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		String nome;
+		int preco;
+		List <Produto> item = new ArrayList<>();
+		
+		for(int i = 0; i<2; i++) {
+			System.out.println("Qual o nome do produto?");
+			nome = read.next();
+			System.out.println("Qual o preço dele?");
+			preco = read.nextInt();
+			item.add(new Produto(nome, preco));
+		}
+		System.out.println("------------------------------");
+		for(Produto estoque: item ) {
+			System.out.println(estoque.getNome());
+		}
+		System.out.println("------------------------------");
+		item.remove(1);
+		System.out.println("LISTA ATUALIZADA:");
+		for(Produto estoque: item ) {
+			System.out.println(estoque.getNome());
+		}
+	}
+
+}
---
+++ java/AULAS/src/listaHerança/Produto.java
@@ -0,0 +1,34 @@
+package listaHerança;
+
+public class Produto {
+	
+	private String nome;
+	private double preco;
+	private int qtdProduto;
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQtdProduto() {
+		return qtdProduto;
+	}
+	public void setQtdProduto(int qtdProduto) {
+		this.qtdProduto = qtdProduto;
+	}
+	
+	
+
+}
